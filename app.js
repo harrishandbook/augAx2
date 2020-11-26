a68c38b4bef116b18d4c7c3c2a9523252782f16c
harrishandbook <harrishandbook@users.noreply.github.com>
2020-11-26 07:33:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+if ( location.protocol != "https:" ) {
+location.href = "https:" + window.location.href.substring( window.location.protocol.length );
+}
+function permission () {
+    if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+        // (optional) Do something before API request prompt.
+        DeviceMotionEvent.requestPermission()
+            .then( response => {
+            // (optional) Do something after API prompt dismissed.
+            if ( response == "granted" ) {
+                window.addEventListener( "devicemotion", (e) => {
+                    // do something for 'e' here.
+                })
+            }
+        })
+            .catch( console.error )
+    } else {
+        alert( "DeviceMotionEvent is not defined" );
+    }
+}
+const btn = document.getElementById( "request" );
+btn.addEventListener( "click", permission );
+// END OF NEW ACCESS REQUEST XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "environment" }, audio: false };
 // Define constants
